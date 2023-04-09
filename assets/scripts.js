3e23b629b2909b502e3e19ec794b670d02583ac5
Ki Sar <rxs291@yahoo.com>
2023-04-09 07:07:11 -0500

    Post Tutoring session: global variable established and working eventListeners

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,50 +2,115 @@ var startQuiz = document.querySelector("#start");
 var containerDisplay = document.querySelector(".container");
 var cardTop = document.querySelector("#top");
 var cardMid = document.querySelector("#mid");
-var cardBot = document.querySelector("#bot");
-var allButtons = document.querySelectorAll("#btn");
+var cardBot = document.querySelector("#bot"); 
+var timer = document.querySelector('#timer');
+var secondsLeft = 80;
+var listOptions = document.querySelector('#listOptions')
+var index = 0;
+var timerInterval
+
 
 var arrOptions = ['round', "square", "UGLy"];
+var arrGlobal = [{
+    question: "9087430985723894057",
+    option1:9,
+    option2:7,
+    option3:6,
+    option4:5,
+    ANSWER:5
+},
+{
+    question: "lakjlkjdsf",
+    option1:3,
+    option2:4,
+    option3:5,
+    option4:6,
+    ANSWER:3
+}
+];
+
  
 startQuiz.addEventListener("click", function(){
 
-    cardBot.textContent = "";
 
-    cardTop.textContent = "First Question: What is shape is the Earth?"
-    cardMid.textContent = "";
+    setTime();
+    displayQuestion();
 
-for (var i = 0; i<arrOptions.length; i++){
 
-    var li = document.createElement("li");
-    var button = document.createElement("button");
-    li.textContent = arrOptions[i];
-    button.textContent = "select";
 
-    cardMid.appendChild(li);
-    li.appendChild(button);
-    button.setAttribute("id","btn")
-}
+})
 
-allButtons.addEventListener("click", function(event){
+
+
+
+
+
+
+
+function btnsEventListner(event){
     var element = event.target;
+ 
 
-    console.log(element.parentElement.textContent)
-    console.log(event.target.parentElement.id);
-
-    if(element.parentElement.textContent.include("round")){
+    if(element.parentElement.textContent.includes(arrGlobal[index].ANSWER)){
         cardBot.textContent = "Correct!";
     }
     else{
         cardBot.textContent = "WRONG!";
+
     }
-})
+
+    index++;
+    if(index<arrGlobal.length){
+        setTimeout(displayQuestion, 500);
+
+    }else{
+        clearInterval(timerInterval);
 
 
-})
+    }
 
 
+}
 
 
+function setTime() { 
+
+    timerInterval = setInterval(function() {
+      secondsLeft--;
+      timer.setAttribute("style", "font-size: 80px")
+      timer.textContent = secondsLeft;
+  
+      if(secondsLeft === 0) { 
+        clearInterval(timerInterval);  
+      }
+  
+    }, 1000);
+}
+
+function displayQuestion(){
+    
+    cardBot.textContent = "";
+
+    cardTop.textContent = arrGlobal[index].question;
+    cardMid.textContent = "";
+
+    listOptions.textContent = "";
+
+for (var i = 1; i<5; i++){
+
+    var li = document.createElement("li");
+    var button = document.createElement("button");
+
+    button.classList.add("btns")
+    li.textContent = arrGlobal[index]["option" + i];
+    button.textContent = "select";
+ 
+    li.appendChild(button); 
+    listOptions.appendChild(li);
+    button.addEventListener("click", btnsEventListner);
+    };
+
+}
 
 
 // GIVEN I am taking a code quiz
