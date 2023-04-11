00c4088bcd1efb45a57ae9d24bd91e932b9e2cd8
Ki Sar <rxs291@yahoo.com>
2023-04-11 15:39:53 -0500

    Almost done, just need to list the highscores

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,23 +4,49 @@ var cardTop = document.querySelector("#top");
 var cardMid = document.querySelector("#mid");
 var cardBot = document.querySelector("#bot"); 
 var timer = document.querySelector('#timer');
+var hidden = document.querySelector('#hidden');
+var save = document.querySelector("#save");
 var secondsLeft = 80;
 var listOptions = document.querySelector('#listOptions')
 var index = 0;
-var timerInterval
+var timerInterval; 
+var highscore = 0;
 
-
-var arrOptions = ['round', "square", "UGLy"];
+ 
 var arrGlobal = [{
-    question: "9087430985723894057",
+    question: "On a single team, how many players are on a volleyball court?",
     option1:9,
     option2:7,
     option3:6,
     option4:5,
+    ANSWER:6
+},
+{
+    question: "The answer is 3",
+    option1:3,
+    option2:4,
+    option3:5,
+    option4:6,
+    ANSWER:3
+},
+{
+    question: "The answer is 4",
+    option1:3,
+    option2:4,
+    option3:5,
+    option4:6,
+    ANSWER:4
+},
+{
+    question: "The answer is 5",
+    option1:3,
+    option2:4,
+    option3:5,
+    option4:6,
     ANSWER:5
 },
 {
-    question: "lakjlkjdsf",
+    question: "The answer is 6",
     option1:3,
     option2:4,
     option3:5,
@@ -31,6 +57,7 @@ var arrGlobal = [{
 
  
 startQuiz.addEventListener("click", function(){
+    
 
 
     setTime();
@@ -56,6 +83,7 @@ function btnsEventListner(event){
     }
     else{
         cardBot.textContent = "WRONG!";
+        secondsLeft -= 15;
 
     }
 
@@ -64,8 +92,9 @@ function btnsEventListner(event){
         setTimeout(displayQuestion, 500);
 
     }else{
+        highscore = secondsLeft;
         clearInterval(timerInterval);
-
+        displayInputBox();
 
     }
 
@@ -81,10 +110,13 @@ function setTime() {
       timer.textContent = secondsLeft;
   
       if(secondsLeft === 0) { 
+        highscore = secondsLeft;
         clearInterval(timerInterval);  
+        displayInputBox()
       }
   
     }, 1000);
+
 }
 
 function displayQuestion(){
@@ -112,14 +144,46 @@ for (var i = 1; i<5; i++){
 
 }
 
+function displayInputBox(){
+    cardTop.textContent = '';
+    cardMid.textContent = "";
+    cardBot.textContent = "";
+    listOptions.textContent= "";
 
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
+    cardTop.textContent = "Please Enter your Initials!";
+    hidden.style.display = "block";
+
+    save.addEventListener('click', function(){
+        var initials = document.querySelector('#initials').value;
+        var yourHighscore = {
+            Initals: initials,
+            highscore: highscore
+        }  
+
+        var savedScores = JSON.parse(localStorage.getItem("savedHighScores")) || []; 
+
+        savedScores.push(yourHighscore); 
+
+        localStorage.setItem("savedHighScores", JSON.stringify(savedScores));  
+
+        console.log(localStorage.getItem("savedHighScores"));
+
+
+      });
+
+
+}
+ 
+ 
+
+
+//!!!GIVEN I am taking a code quiz
+//!!!! WHEN I click the start button
+//!!!!THEN a timer starts and I am presented with a question
+//!!!! WHEN I answer a question
+//!!! THEN I am presented with another question
+//!!!! WHEN I answer a question incorrectly
+//!!!! THEN time is subtracted from the clock
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
 // WHEN the game is over
